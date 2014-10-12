90d885ef160e149b035c0ef4ff6a6a92e569e91c
mtejedorg <m.tejedorg@alumnos.urjc.es>
2014-10-12 11:53:48 -0700

    Implementado karaoke.py y finalizado smallsmilhandler.py. No funciona. La vida es dura

--- karaoke.py
+++ karaoke.py
@@ -2,3 +2,23 @@
 # -*- coding: iso-8859-15 -*-
 
 import smallsmilhandler
+
+com = sys.argv
+
+if len(com) == 2:
+
+    parser = make_parser()
+    sHandler = SmallSMILHandler()
+    parser.setContentHandler(sHandler)
+    try:
+        parser.parse(open(com[1]))
+
+    tags = sHandler.get_tags()
+    
+    print tags
+
+    except IOError:
+        print "Error: Document not found"
+ 
+else:
+    print "Usage: python karaoke.py file.smil"
--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -74,3 +74,4 @@ if __name__ == "__main__":
     parser.parse(open('karaoke.smil'))
 
 
+
